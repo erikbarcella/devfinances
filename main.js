29669741005ffb855c91d2f6a93fb029cab0cd68
Erik Barcella <57962093+ErikBarcellaT@users.noreply.github.com>
2021-03-24 21:04:53 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-Modal ={
+const Modal ={
     open() {
         //abrir modal 
         //adicionar classe active ao modal
+        document.querySelector(".modal-overlay")
+        .classList.add("active")
     },
     close(){
         //fechar modal
         //remover classe modal 
+        document.querySelector(".modal-overlay")
+        .classList.remove("active")
     }
 }
