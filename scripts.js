aeb7a0c2e2ca2dc8e1d57e6f08d95e44b18516ee
Erik Barcella <57962093+ErikBarcellaT@users.noreply.github.com>
2021-05-19 18:19:18 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -136,51 +136,20 @@ const Utils = {   //responsavel por fazer a devida formatação na moeda corrent
         return signal+value
     }
 }
+
 const Form = {
-    description: document.querySelector("input#description"),
-    amount: document.querySelector("input#amount"),
-    date: document.querySelector("input#date"),
 
-    getValues() {
-        return {
-            description: Form.description.value,
-            amount: Form.amount.value,
-            date: Form.date.value
-        }
-    },
-
-    validateFields() {
-        const { description, amount, date} = Form.getValues()
-
-        if( description.trim() === "" || 
-            amount.trim() ==="" || 
-            date.trim() ==="") {
-                throw new Error ("Por favor, preencha todos os campos")
-        }
-
-    },
 
     submit(event) {
         event.preventDefault()
 
-
-        try {
-            Form.validateFields
-            //formatar os dados para salvar
-            //salvar
-            //limpar os campos do form
-            //fechar modal
-            //atualizar toda a aplicação
-            
-        } catch (error) {
-            
-        }
-
-       
-    },
-
-
-
+        //verificar se foi tudo preenchido
+        // formatar os dados
+        //salvar os dados
+        //limpar o form
+        //fechar modal
+        //atualizar app
+    }
 }
 
 const App ={
